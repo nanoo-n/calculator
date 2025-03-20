3101b520b6e16f557f1115b87657affca2ed3bf1
nanoo-n <88988610+nanoo-n@users.noreply.github.com>
2025-03-20 17:22:03 -0300

    play with DOM manipulation

--- code.js
+++ code.js
@@ -1,17 +1,22 @@
-console.log("hello world");
-
+//Initial declarations
 //let userInput = prompt("What's your calculation?");
 let inputArray;
 let firstNumber = 0;
 let secondNumber = 0;
-let operator;
+let operation;
 let result = 0;
+
+//querySelectors
+const display = document.querySelector("#textbox");
+const zero = document.querySelector("#zero");
+const one = document.querySelector("#one")
+const two = document.querySelector("#two")
 
 function stripInput(userInput) {
     inputArray = userInput.split(/([^\d.])/);
     firstNumber = parseInt(inputArray[0]);
     secondNumber = parseInt(inputArray[2]);
-    operator = inputArray[1];
+    operation = inputArray[1];
 };
 
 //stripInput(userInput);
@@ -32,18 +37,32 @@ function divide() {
     result = firstNumber / secondNumber;
 }
 
-//console.log(`Operator: ${operator}`);
-//console.log(`Initial result: ${result}`);
-
-if (operator == "+") {
-    add();
-} else if (operator == "-") {
-    subtract();
-} else if (operator == "*") {
-    multiply();
-} else if (operator == "/") {
-    divide();
+function operator(sign) {
+    if (sign == "+") {
+        add();
+    } else if (sign == "-") {
+        subtract();
+    } else if (sign == "*") {
+        multiply();
+    } else if (sign == "/") {
+        divide();
+    }
 }
+
+operator(operation);
+
+//DOM Manipulation
+zero.addEventListener("click", () => {
+    display.value = "ZERO";
+})
+
+one.addEventListener("click", ()=>{
+    display.value = "ONE"
+})
+
+two.addEventListener("click", ()=>{
+    display.value = "TWO"
+})
 
 console.log(`After: ${inputArray}`);
 console.log(`Result: ${result}`);
