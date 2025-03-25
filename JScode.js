6f811cc150e440d941194f82ccefe0ecbfd221a9
nanoo-n <88988610+nanoo-n@users.noreply.github.com>
2025-03-25 14:08:50 -0300

    refactor script to move from arrays into variables

---
+++ JScode.js
@@ -0,0 +1,88 @@
+        //*querySelectors
+const display = document.querySelector("#textbox");
+const equals = document.querySelector("#equals");
+const clear = document.querySelector("#clear");
+const buttons = document.querySelector("#buttons");
+const operators = document.querySelector("#operators");
+
+        //*initial declarations
+let firstNumber = "";
+let secondNumber = "";
+let result = 0;
+let operator;
+
+        //*event listeners
+buttons.addEventListener("click", (e) => {
+    const target = e.target;
+
+    if (target.tagName === 'BUTTON') {
+        if (operator == undefined) {
+            firstNumber = firstNumber.concat("", target.textContent);
+            display.value = firstNumber;
+        }
+        if (operator !== undefined) {
+            secondNumber = secondNumber.concat("", target.textContent);
+            display.value = secondNumber;
+        }
+    }
+});
+
+operators.addEventListener("click", (e) => {
+    const target = e.target;
+
+    if (target.tagName === 'BUTTON') {
+        operator = target.textContent;
+        display.value = operator;
+    }
+})
+
+equals.addEventListener("click", () => {
+    operation(operator);
+    display.value = result.toFixed(2);
+    console.log(`${result}`);
+    clearNumbers();
+})
+
+clear.addEventListener("click", () => {
+    result = 0;
+    firstNumber = "";
+    secondNumber = "";
+    operator = undefined;
+    display.value = "0";
+})
+
+        //*operation functions
+function add() {
+    result = parseFloat(firstNumber) + parseFloat(secondNumber);
+};
+
+function subtract() {
+    result = parseFloat(firstNumber) - parseFloat(secondNumber);
+}
+
+function multiply() {
+    result = parseFloat(firstNumber) * parseFloat(secondNumber);
+}
+
+function divide() {
+    result = parseFloat(firstNumber) / parseFloat(secondNumber);
+}
+
+function operation(sign) {
+    console.log(`firstNumber ${firstNumber} ${operator} secondNumber ${secondNumber}`);
+    if (sign == "+") {
+        add();
+    } else if (sign == "-") {
+        subtract();
+    } else if (sign == "*") {
+        multiply();
+    } else if (sign == "/") {
+        divide();
+    }
+}
+
+function clearNumbers() {
+    firstNumber = result;
+    secondNumber = "";
+    operator = undefined;
+}
